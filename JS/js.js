b4cdccd4e49d01e8409235db87543027b1c6c5cd
Catarina Cruz <CatarinaC@MacBook-Pro-de-Catarina-8.local>
2017-07-05 13:20:33 +0100

    Homepage

---
+++ JS/js.js
@@ -0,0 +1,10 @@
+
+function mostrarProdutosMenu(){
+    var div = document.getElementById('produtosMenu');
+    
+    if(div.style.display === 'none'){
+        div.style.display = 'block';
+    }else{
+        div.style.display = 'none';
+    }
+}
